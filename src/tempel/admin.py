a06c64d1da907e94a1154128d7768be57c136c84
Fajran Iman Rusadi <fajran@gmail.com>
2010-09-13 10:26:04 +0200

    Show IP address of the creator of an entry on the admin page

--- src/tempel/admin.py
+++ src/tempel/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from tempel.models import Entry
 
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'language', 'created', 'active']
+    list_display = ['id', 'language', 'created', 'ip', 'active']
 
 admin.site.register(Entry, EntryAdmin)
 
