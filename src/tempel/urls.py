635163977f1aad50a799042422ea81571eb0b583
Fajran Iman Rusadi <fajran@gmail.com>
2010-09-13 10:15:06 +0200

    Add URLs for viewing private entry

--- src/tempel/urls.py
+++ src/tempel/urls.py
@@ -15,6 +15,10 @@ urlpatterns = patterns('',
     url(r'^entry/(?P<id>\d+)/download/$', 'tempel.views.download', name='tempel_download'),
     url(r'^entry/(?P<id>\d+)/edit/(?P<token>\w{8})/$', 'tempel.views.edit', name='tempel_edit'),
 
+    url(r'^(?P<id>\d+).(?P<private_token>\w{8})$', 'tempel.views.private_view', {'mode': 'html'}, name='tempel_private_view'),
+    url(r'^(?P<id>\d+).(?P<private_token>\w{8}).html$', 'tempel.views.private_view', {'mode': 'html'}, name='tempel_private_html'),
+    url(r'^(?P<id>\d+).(?P<private_token>\w{8}).txt$', 'tempel.views.private_view', {'mode': 'txt'}, name='tempel_private_raw'),
+
     url(r'^entry/(?P<id>\d+).(?P<private_token>\w{8})/download/$', 'tempel.views.private_download', name='tempel_private_download'),
 
     url(r'^$', 'tempel.views.index', name='tempel_index'),
